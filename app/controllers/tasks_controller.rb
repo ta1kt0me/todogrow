76dfdbb0a5031d75d04d784857b956c3ca45a82b
ta1kt0me <p.wadachi@gmail.com>
2014-09-25 17:11:21 +0900

    add tasks#create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,20 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to :root
+    else
+      render :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(
+      :name, :deadline
+    )
+  end
 end
