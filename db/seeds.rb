10e795e5b6d77dee6afc987afcbf75427f16912e
ta1kt0me <p.wadachi@gmail.com>
2014-09-25 16:38:15 +0900

    create seeds test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Task.delete_all
+Task.create(name: 'タスク1', deadline: DateTime.new(2014,9,1,0,0,0))
+Task.create(name: 'タスク2', deadline: DateTime.new(2014,10,1,0,0,0))
+Task.create(name: 'タスク3', deadline: DateTime.new(2014,11,1,0,0,0))
+Task.create(name: 'タスク4', deadline: DateTime.new(2014,12,1,0,0,0))
