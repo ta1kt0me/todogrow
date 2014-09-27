d7b956d1a4eebc0765bb37100d92acaacf4e7b93
ta1kt0me <p.wadachi@gmail.com>
2014-09-27 13:47:45 +0900

    add close_index in TasksController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :tasks
+  get '/tasks/closed', to: 'tasks#closed_index', as: 'closed_tasks'
   post '/tasks/:id/close', to: 'tasks#close', as: 'close_task'
   root :to => 'tasks#index'
 end
