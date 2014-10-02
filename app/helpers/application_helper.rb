f62a7943013426da262568df8fdc7aa019884c57
ta1kt0me <p.wadachi@gmail.com>
2014-10-03 03:31:52 +0900

    add display alert close button method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def alert_close_icon
+    haml_tag(:button, class: 'close', type: 'button', 'data-dismiss' => 'alert', 'aria-hidden' => 'true') do
+      haml_concat '&times;'
+    end
+  end
 end
