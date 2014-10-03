3ac2e19c2df66a7cc35f84a6340e3efa4f061bf9
ta1kt0me <p.wadachi@gmail.com>
2014-10-03 12:15:25 +0900

    add tag in model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
+  acts_as_taggable
+
   belongs_to :user
 
   validates :name, length: {maximum: 150}, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
+  acts_as_tagger
 
   def self.create_with_omniauth(auth)
     where(provider: auth['provider'], uid: auth['uid']).first_or_create do |user|
