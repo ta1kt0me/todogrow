3a3ec8ad1260387433565813d9b30388007de424
ta1kt0me <p.wadachi@gmail.com>
2014-10-29 19:26:17 +0900

    add tasks#reopen

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/tasks/closed', to: 'tasks#closed_index', as: 'closed_tasks'
   resources :tasks
   post '/tasks/:id/close', to: 'tasks#close', as: 'close_task'
+  post '/tasks/:id/reopen', to: 'tasks#reopen', as: 'reopen_task'
   root to: 'tasks#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy', as: 'logout'
